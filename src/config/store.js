0e7089d5d72d47ff834bbe059d573817d419251c
Mutahhar Bin Muzaffar <mutahhar.muzaffar@devsinc.com>
2022-06-27 17:43:40 +0500

    Refactored code

--- src/config/store.js
+++ src/config/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import loadingReducer from '../reducers/loading'
 import usersReducer from '../reducers/users'
 
 export default configureStore({
   reducer: {
-    loader: loadingReducer,
     users: usersReducer
   }
 })
